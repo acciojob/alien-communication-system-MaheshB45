86b5074346737057d7e3a8b7c925bdc136973072
Mahesh Rajendra Bahir <107680212+MaheshB45@users.noreply.github.com>
2024-01-13 11:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+function log(message) {
+  const outputDiv = document.getElementById('output');
+  outputDiv.innerHTML += message + '<br>';
+}
+
+log('Start of Program');
+
+/* Macrotask (setTimeout)*/
+setTimeout(() => {
+  log('Macrotask 1 (setTimeout)');
+}, 0);
+
+/* Macrotask (setTimeout)*/
+setTimeout(() => {
+  log('Macrotask 2 (setTimeout)');
+}, 0);
+
+/* Microtask (Promise.resolve().then)*/
+Promise.resolve().then(() => {
+  log('Microtask 1 (Promise.resolve().then)');
+});
+
+/* Microtask (Promise.resolve().then)*/
+Promise.resolve().then(() => {
+  log('Microtask 2 (Promise.resolve().then)');
+});
+
+log('End of Program');
